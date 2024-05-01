24a97b0a8ad5b732eaa711ae06fa0edcf1b1be0c
Sebastianbotv <sebastianbotero85@gmail.com>
2024-05-01 17:48:25 -0500

    calculadora con varios metodos para sus operaciones

---
+++ script.js
@@ -0,0 +1,192 @@
+let teclas;
+let pantalla = document.getElementById("pantalla");
+let clear; 
+let igual;
+let operador;
+let valorUno;
+let valorDos;
+let multiplicar
+let dividir;
+let restar;
+let sumar;
+
+
+window.onload = function () {
+    teclas = document.getElementsByClassName('teclas');
+    agregarEventos(teclas);
+    operador = document.getElementsByClassName('operador');
+    valorUno = this.innerHTML;
+    valorDos = this.innerHTML;
+    multiplicar = document.getElementById("multiplicar");
+    dividir = document.getElementById("dividir");
+    restar = document.getElementById("restar");
+    sumar = document.getElementById("sumar");
+    igual = document.getElementById('igual');
+   
+    clear = document.getElementById("clear");
+    clear.addEventListener(`click`, function () {
+        pantalla.value = ""
+        valorUno = "";
+        valorDos = "";
+    })
+    
+      resolver()
+
+}
+
+
+
+/*TECLADO*/
+
+function agregarEventos(teclas) {
+    for (let i = 0; i < teclas.length; i++) {
+        teclas[i].addEventListener('click', function () {
+            let valor = this.innerHTML;
+        
+            if (pantalla.value.length < 15) {
+
+                pantalla.value += valor;
+
+            }
+
+           
+
+        });
+
+    }
+      
+   
+}
+
+//INTENTO CON ARRAYS
+/*function operaciones(operador) {
+
+    for (let i = 0; i < operador.length; i++) {
+        operador[i].addEventListener('click', function () {
+
+           console.log("oprimiste un operador") 
+          
+        });
+        
+    }
+    
+}*/
+
+/*function resolver() {
+
+
+    let respuesta = 0;
+    switch (operador) {
+        case operador[0]:
+            valorUno = this.innerHTML;
+            respuesta = parseFloat(valorUno) * parseFloat(valorDos);
+            console.log("multiplicacion")
+            
+            break;
+
+            case operador[1]:
+            valorUno = this.innerHTML;
+            respuesta = parseFloat(valorUno) / parseFloat(valorDos);
+            console.log("division")
+
+            break;
+
+            case operador[2]:
+            valorUno = this.innerHTML;
+            respuesta = parseFloat(valorUno) - parseFloat(valorDos);
+            console.log("resta")
+
+            break;
+
+            case operador[3]:
+            valorUno = this.innerHTML;
+            respuesta = parseFloat(valorUno) + parseFloat(valorDos);
+            console.log("suma")
+
+            break;
+       }
+
+    
+    
+}
+
+igual = document.getElementById('igual');
+
+igual.addEventListener('click', function(){
+     valorDos = this.innerHTML;
+    pantalla.value = resolver()
+})*/
+
+//INTENTO CON FUNCIONES Y EVENTOS
+/*multiplicar.onclick = function(e) {
+    valorUno = pantalla.value;
+    operador = "*";
+}
+
+dividir.onclick = function() {
+    valorUno = pantalla.value;
+    operador = "/";
+}
+
+restar.onclick = function() {
+    valorUno = pantalla.value;
+    operador = "-";
+}
+
+sumar.onclick = function() {
+    valorUno = pantalla.value;
+    operador = "+";
+}
+
+
+igual.onclick = function(){
+    valorDos = pantalla.value;
+    resolver()
+
+}
+
+
+ function resolver(){
+
+    let respuesta = "";
+    switch (operador) {
+        case "*":
+            valorUno = pantalla.value;
+            respuesta = parseFloat(valorUno) * parseFloat(valorDos);
+            console.log("multiplicacion")
+            
+            break;
+
+            case "/":
+            valorUno = pantalla.valueL;
+            respuesta = parseFloat(valorUno) / parseFloat(valorDos);
+            console.log("division")
+
+            break;
+
+            case "-":
+            valorUno = pantalla.value;
+            respuesta = parseFloat(valorUno) - parseFloat(valorDos);
+            console.log("resta")
+
+            break;
+
+            case "+":
+            valorUno = pantalla.value;
+            respuesta = parseFloat(valorUno) + parseFloat(valorDos);
+            console.log("suma")
+
+            break;
+       }
+
+       pantalla.value = respuesta;
+
+   
+ }*/
+
+ igual = document.getElementById("igual");
+ igual.addEventListener('click', function(){
+
+    pantalla.value = eval(pantalla.value);
+
+ })
